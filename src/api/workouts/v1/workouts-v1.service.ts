e1cd5037b2c2cf2a5b8f0ad4eba6bd175594aaaf
Joao Afonso Gaspar Lopes <joao.afonso.1995@gmail.com>
2023-01-28 20:46:12 +0100

    Add logger to workouts v1 service

--- src/api/workouts/v1/workouts-v1.service.ts
+++ src/api/workouts/v1/workouts-v1.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 
+import { Logger } from "../../../logging/logger";
 import { WorkoutsRepository } from "../../../repository/workouts.repository";
 import { WorkoutNotFoundException } from "../exceptions/workout-not-found-exception";
 import { WorkoutV1Dto } from "../types/workout-v1.dto";
@@ -7,12 +8,15 @@ import { WorkoutsV1Mapper } from "./workouts-v1.mapper";
 
 @Injectable()
 class WorkoutsV1Service {
+    private readonly logger = new Logger(WorkoutsV1Service.name);
+
     constructor(
         private readonly mapper: WorkoutsV1Mapper,
         private readonly repository: WorkoutsRepository,
     ) {}
 
     getWorkouts(): WorkoutV1Dto[] {
+        this.logger.log("Retrieving all workouts");
         const workoutEntities = this.repository.findAll();
 
         const workouts = workoutEntities.map((workoutEntity) =>
@@ -23,9 +27,13 @@ class WorkoutsV1Service {
     }
 
     getWorkout(id: string): WorkoutV1Dto {
+        this.logger.log(`Retrieving workout with id: ${id}`);
         const workoutEntity = this.repository.findById(id);
 
-        if (!workoutEntity) throw new WorkoutNotFoundException();
+        if (!workoutEntity) {
+            this.logger.error(`No workout found with id: ${id}`);
+            throw new WorkoutNotFoundException();
+        }
 
         return this.mapper.toWorkoutV1Dto(workoutEntity);
     }
