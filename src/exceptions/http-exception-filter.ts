02d46b1ff35a0cb80d95f7ec3b9890d9fde50432
Joao Afonso Gaspar Lopes <joao.afonso.1995@gmail.com>
2023-01-29 10:10:01 +0100

    Improve http exception filter typing

--- src/exceptions/http-exception-filter.ts
+++ src/exceptions/http-exception-filter.ts
@@ -1,13 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
 import { Response } from "express";
-import { buildErrorResponse } from "./error-response";
+import { ErrorResponse, buildErrorResponse } from "./error-response";
 
 @Catch(HttpException)
 class HttpExceptionFilter implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost): void {
         const ctx = host.switchToHttp();
 
-        const res = ctx.getResponse<Response>();
+        const res = ctx.getResponse<Response<ErrorResponse>>();
 
         const statusCode = exception.getStatus();
 
